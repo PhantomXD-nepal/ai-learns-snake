22c27b2eac0621c6407e60920d99e889c5acf1d1
PhantomXD-nepal <phantomarcanexd@gmail.com>
2025-10-01 09:14:23 +0545

    Agent class done todo: Model and helper

--- agent.py
+++ agent.py
@@ -18,6 +18,8 @@ class Agent:
         self.memory = deque(maxlen=MAX_MEMORY)  # popleft()
 
         # TODO: model and trainer
+        self.model = None
+        self.trainer = None
 
     def get_state(self, game):
         head = game.snake[0]
@@ -62,16 +64,39 @@ class Agent:
         return np.array(state, dtype=int)
 
     def remember(self, state, action, reward, next_state, done):
-        
+        self.memory.append((state, action, reward, next_state, done))
 
     def train_long_memory(self):
-        pass
+        if len(self.memory) > BATCH_SIZE:
+            mini_sample = random.sample(self.memory, BATCH_SIZE)
+        else:
+            mini_sample = self.memory
+
+        states, actions, rewards, next_states, dones = zip(*mini_sample)
+        self.trainer.train_step(states, actions, rewards, next_states, dones)
 
     def train_short_memory(self, state, action, reward, next_state, done):
-        pass
+        self.trainer.train_step(state, action, reward, next_state, done)
 
     def get_action(self, state):
-        pass
+        self.epsilon = 80 - self.n_games
+        final_move = [0, 0, 0]
+        if (
+            random.randint(0, 200) < self.epsilon
+        ):  # This makes sure the mrore the games played the less the probability of the random move being called.
+            move = random.randint(0, 2)  # Get a random index
+            final_move[move] = 1  # do a random action
+        else:
+            state0 = torch.tensor(state, dtype=torch.float)
+            prediction = self.model.predict(state0)
+            move = torch.argmax(prediction).item()
+            final_move[move] = 1
+
+        return final_move
+
+        return final_move
+
+)
 
 
 def train():
